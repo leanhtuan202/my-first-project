be08f976bf3eab9a2872c0a3097544522f7db6f5
ngoanhtien <ngoanhtien902@gmail.com>
2024-11-10 00:04:05 +0700

    feat(UserService): add handleFindUserById, findAllUsers, handleUpdateUser method

--- src/main/java/vn/ngotien/jobhunter/service/UserService.java
+++ src/main/java/vn/ngotien/jobhunter/service/UserService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 import vn.ngotien.jobhunter.domain.User;
 import vn.ngotien.jobhunter.repository.UserRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -18,5 +21,28 @@ public class UserService {
 
     public void handleDeleteUser(long id) {
         this.userRepository.deleteById(id);
+    }
+
+    public User handleFindUserById(long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    public List<User> findAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    public void hanldleUpdateUser(User user) {
+        //Lay id user can update
+        long id = user.getId();
+        //Lay ra user tu db
+        Optional<User> userOptional = this.userRepository.findById(id);
+        //Sua thong tin
+        if (userOptional.isPresent()) {
+            userOptional.get().setName(user.getName());
+            userOptional.get().setEmail(user.getEmail());
+            userOptional.get().setPassword(user.getPassword());
+            this.userRepository.save(userOptional.get());
+        }
     }
 }
